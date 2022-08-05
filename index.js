95f4a3f5e3707a071de99f89bb4e523e69d9fb5c
TheWebDog <2725550609@qq.com>
2022-08-05 17:32:55 +0800

    1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
+
+// const VercelRequest = require('@vercel/node').VercelRequest;
+// const VercelResponse = require('@vercel/node').VercelResponse;
+
+// module.exports = async (req: VercelRequest, res: VercelResponse) => {
+//     const data = {
+//         msg: "hello world!"
+//     };
+//     res.status(200).json(data);
+// }
+
+
 // 解决跨域问题
-const cors = require('cors')
-app.use(cors())
+// const cors = require('cors')
+// app.use(cors())
 
 // 解析post请求发来的数据
 app.use(express.urlencoded({extended:false}))
