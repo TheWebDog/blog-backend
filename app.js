6b775b13b3d27c9ac9ed0df0ec783dc12ecc371f
TheWebDog <2725550609@qq.com>
2022-08-05 16:10:07 +0800

    添加测试接口

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const cors = require('cors')
 const app = express();
 
 // 解决跨域问题
-app.use(cors())
+// const cors = require('cors')
+// app.use(cors())
 
 // 解析post请求发来的数据
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.send('链接成功')
+})
+
 // 分路由
 app.use('/page', require('./routers/pageRouter'))
 app.use('/user', require('./routers/userRouter'))
 
-app.listen(4000)
+app.listen(6060)
