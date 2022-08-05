bf7c52df46d5d5ad698028e357d5ad2fb469dd6e
TheWebDog <2725550609@qq.com>
2022-08-06 02:45:18 +0800

    测试

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ const app = express();
 // const cors = require('cors')
 // app.use(cors())
 
+// 网易云项目抄来的
+app.set('trust proxy', true)
+app.use((req, res, next) => {
+  if (req.path !== '/' && !req.path.includes('.')) {
+    res.set({
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': req.headers.origin || '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+      'Content-Type': 'application/json; charset=utf-8',
+    })
+  }
+  req.method === 'OPTIONS' ? res.status(204).end() : next()
+})
+
+
 // 解析post请求发来的数据
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
@@ -25,8 +41,8 @@ app.get('/', (req, res) => {
   res.send('链接成功')
 })
 
-// 分路由
-app.use('/page', require('./routers/pageRouter'))
-app.use('/user', require('./routers/userRouter'))
+// // 分路由
+// app.use('/page', require('./routers/pageRouter'))
+// app.use('/user', require('./routers/userRouter'))
 
 app.listen(6060)
